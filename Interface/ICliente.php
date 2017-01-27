91f33ea22d29e7b913dd537dff736ce9df995644
Adriano A Costa <sdcomputadores@gmail.com>
2017-01-27 13:44:49 -0200

    update na interface de cliente

--- Interface/ICliente.php
+++ Interface/ICliente.php
@@ -4,18 +4,11 @@ interface ICliente
 {
 
     public function setId($id);
-
     public function getId();
-
     public function getNome();
-
     public function setNome($nome);
-
     public function getEmail();
-
     public function setEmail($email);
-
     public function getSenha();
-
     public function setSenha($senha);
 }
