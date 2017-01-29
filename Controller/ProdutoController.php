27983434df94869fe267d12e11686dbd452951d0
Adriano A Costa <sdcomputadores@gmail.com>
2017-01-28 22:34:50 -0200

    criando a produto controller

--- Controller/ProdutoController.php
+++ Controller/ProdutoController.php
@@ -1,11 +1,17 @@
 <?php
 
-class ProdutoController
-{
+class ProdutoController {
 
-    public function index()
-    {
-        echo "produto index";
+    private $model;
+    private $library;
+
+    public function __construct() {
+        $this->model = new ProdutoModel();
+        $this->library = new Library();
+    }
+
+    public function index() {
+        echo "index ProdutoController";
     }
 
 }
